f424f4bad2460aa4c47056e46c50279ed30c764a
Sven Lüppken <sven@kde.org>
2002-02-14 20:09:13 +0000

    Ported to KDE3 Patch by Holger Schröder <holger@holgis.net>

---
+++ lib/koscript/koscript_locale.cc
@@ -0,0 +1,16 @@
+#include "koscript_locale.h"
+
+KSLocale::KSLocale() :
+	KLocale("koffice")
+{
+    setDecimalSymbol( "." );
+    setThousandsSeparator( "" );
+    setMonetaryDecimalSymbol( "." );
+    setMonetaryThousandsSeparator( "" );
+    setPositiveSign( "" );
+    setNegativeSign( "-" );
+    setWeekStartsMonday( false );
+    setTimeFormat("%I:%M:%S %p");
+    setDateFormat("%A %d %B %Y");
+    setDateFormatShort( "%m/%d/%y" );
+}
