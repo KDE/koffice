43e7f65b3b07983c25d4ffc16172af0a4dfea4a1
Nicolas Goutte <nicolasg@snafu.de>
2003-09-30 10:52:05 +0000

    Work around Qt Bug for monochrome images (Backport, bug #30357)

---
+++ lib/kofficecore/koPictureImage.cc
@@ -0,0 +1,178 @@
+/* This file is part of the KDE project
+   Copyright (c) 2001 Simon Hausmann <hausmann@kde.org>
+   Copyright (C) 2002 Nicolas GOUTTE <nicog@snafu.de>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#include <qbuffer.h>
+#include <qpainter.h>
+#include <qimage.h>
+#include <qpixmap.h>
+#include <qapplication.h>
+
+#include <kdebug.h>
+#include <kmimetype.h>
+
+#include "koPictureKey.h"
+#include "koPictureBase.h"
+#include "koPictureImage.h"
+
+KoPictureImage::KoPictureImage(void) : m_cacheIsInFastMode(true)
+{
+    // Forbid QPixmap to cache the X-Window resources (Yes, it is slower!)
+    m_cachedPixmap.setOptimization(QPixmap::MemoryOptim);
+}
+
+KoPictureImage::~KoPictureImage(void)
+{
+}
+
+KoPictureBase* KoPictureImage::newCopy(void) const
+{
+    return new KoPictureImage(*this);
+}
+
+KoPictureType::Type KoPictureImage::getType(void) const
+{
+    return KoPictureType::TypeImage;
+}
+
+bool KoPictureImage::isNull(void) const
+{
+    return m_originalImage.isNull();
+}
+
+void KoPictureImage::scaleAndCreatePixmap(const QSize& size, bool fastMode)
+{
+    if ((size==m_cachedSize)
+        && ((fastMode) || (!m_cacheIsInFastMode)))
+    {
+        // The cached pixmap has already the right size
+        // and:
+        // - we are in fast mode (We do not care if the re-size was done slowly previously)
+        // - the re-size was already done in slow mode
+        return;
+    }
+
+    // Slow mode can be very slow, especially at high zoom levels -> configurable
+    if ( !isSlowResizeModeAllowed() )
+    {
+        kdDebug(30003) << "User has disallowed slow mode!" << endl;
+        fastMode = true;
+    }
+
+    // Use QImage::scale if we have fastMode==true
+    QImage img;
+    if ( fastMode )
+    {
+        img = m_originalImage.scale( size );
+        m_cacheIsInFastMode=true;
+    }
+    else
+    {
+        img = m_originalImage.smoothScale( size );
+        m_cacheIsInFastMode=false;
+    }
+    // Work around Qt 3.x Bug by using QPixmap::Color instead of Auto
+    m_cachedPixmap.convertFromImage(img, QPixmap::Color);
+    m_cachedSize=size;
+}
+
+void KoPictureImage::draw(QPainter& painter, int x, int y, int width, int height, int sx, int sy, int sw, int sh, bool fastMode)
+{
+    //kdDebug() << "KoImage::draw currentSize:" << currentSize.width() << "x" << currentSize.height() << endl;
+    if ( !width || !height )
+        return;
+    QSize origSize = getOriginalSize();
+    const bool scaleImage = painter.device()->isExtDev() // we are printing
+        && ((width <= origSize.width()) || (height <= origSize.height()));
+    if( scaleImage )
+    {
+        // use full resolution of image
+        double xScale = double(width) / double(origSize.width());
+        double yScale = double(height) / double(origSize.height());
+
+        painter.save();
+        painter.translate( x, y );
+        painter.scale( xScale, yScale );
+        // Note that sx, sy, sw and sh are unused in this case. Not a problem, since it's about printing.
+        // Note 2: we do not cache the QPixmap. As we are printing, the next time we will probably
+        //   need again the screen version.
+        painter.drawPixmap(0, 0, QPixmap(m_originalImage));
+        painter.restore();
+
+    }
+    else
+    {
+        QSize screenSize( width, height );
+        //kdDebug() << "KoPictureImage::draw screenSize=" << screenSize.width() << "x" << screenSize.height() << endl;
+
+        scaleAndCreatePixmap(screenSize, fastMode);
+
+        // sx,sy,sw,sh is meant to be used as a cliprect on the pixmap, but drawPixmap
+        // translates it to the (x,y) point -> we need (x+sx, y+sy).
+        painter.drawPixmap( x + sx, y + sy, m_cachedPixmap, sx, sy, sw, sh );
+    }
+}
+
+bool KoPictureImage::load(QIODevice* io, const QString& /*extension*/)
+{
+    // First, read the raw data
+    m_rawData=io->readAll();
+
+    // Second, create the original image
+    QBuffer buffer(m_rawData);
+    buffer.open(IO_ReadWrite);
+    QImageIO imageIO(&buffer,NULL);
+
+    if (!imageIO.read())
+    {
+        buffer.close();
+        return false;
+    }
+    buffer.close();
+    m_originalImage=imageIO.image();
+
+    return true;
+}
+
+bool KoPictureImage::save(QIODevice* io)
+{
+    // We save the raw data, to avoid damaging the file by many load/save cyvles (especially for JPEG)
+    Q_ULONG size=io->writeBlock(m_rawData); // WARNING: writeBlock returns Q_LONG but size() Q_ULONG!
+    return (size==m_rawData.size());
+}
+
+QSize KoPictureImage::getOriginalSize(void) const
+{
+    return m_originalImage.size();
+}
+
+QPixmap KoPictureImage::generatePixmap(const QSize& size, bool smoothScale)
+{
+    scaleAndCreatePixmap(size,!smoothScale);
+    return m_cachedPixmap;
+}
+
+QString KoPictureImage::getMimeType(const QString& extension) const
+{
+    QString fileName("/tmp/temp.");
+    fileName+=extension;
+    QString mimetype=KMimeType::findByURL(fileName,0,true,true)->name();
+    kdDebug(30003) << "Image is mime type: " << mimetype << endl;
+    return mimetype;
+}
