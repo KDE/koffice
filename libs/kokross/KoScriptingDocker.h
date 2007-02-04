24063f10ca0c287d830f8b29f4a293289f0c62c7
Sebastian Sauer <mail@dipe.org>
2007-02-04 00:46:02 +0000

    Added the kokross directory to libs which provides us within KOffice reused functionality like the ScriptingDocker. koffice/libs/kokross extends kdelibs/kross, koffice/libs/kross will be moved to kdebindings later.

---
+++ libs/kokross/KoScriptingDocker.h
@@ -0,0 +1,62 @@
+/*
+ * This file is part of Krita
+ *
+ * Copyright (C) 2006-2007 by Sebastian Sauer (mail@dipe.org)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef KOSCRIPTINGDOCKER_H
+#define KOSCRIPTINGDOCKER_H
+
+#include <QDockWidget>
+#include <KoDockFactory.h>
+
+namespace Kross {
+    class GUIClient;
+}
+
+#define KOKROSS_EXPORT KDE_EXPORT
+
+class KOKROSS_EXPORT KoScriptingDockerFactory : public KoDockFactory
+{
+    public:
+        KoScriptingDockerFactory(QWidget* parent, Kross::GUIClient* guiclient);
+        virtual ~KoScriptingDockerFactory();
+        Kross::GUIClient* guiClient() const;
+        virtual QString dockId() const;
+        virtual Qt::DockWidgetArea defaultDockWidgetArea() const;
+        virtual QDockWidget* createDockWidget();
+    private:
+        class Private;
+        Private* const d;
+};
+
+class KOKROSS_EXPORT KoScriptingDocker : public QDockWidget
+{
+        Q_OBJECT
+    public:
+        KoScriptingDocker(QWidget* parent, Kross::GUIClient* guiclient);
+        virtual ~KoScriptingDocker();
+        Kross::GUIClient* guiClient() const;
+    private slots:
+        void runScript();
+        void stopScript();
+    private:
+        class Private;
+        Private* const d;
+};
+
+#endif
