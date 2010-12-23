83cc2c870cf66910be26cd689f9b476f855856c8
Thomas Zander <zander@kde.org>
2010-12-23 15:57:35 +0000

    Its a new version

--- libs/main/kofficeversion.h
+++ libs/main/kofficeversion.h
@@ -33,7 +33,7 @@
 * it contains the version at compile time. See KOffice::versionString() if you need
 * the KOffice version used at runtime.
 */
-#define KOFFICE_VERSION_STRING "2.3 Beta 4"
+#define KOFFICE_VERSION_STRING "2.4 Alpha"
 
 /**
  * @def KOFFICE_VERSION_MAJOR
@@ -47,14 +47,14 @@
  * @ingroup KOfficeMacros
  * @brief Minor version of KOffice, at compile time
 */
-#define KOFFICE_VERSION_MINOR 2
+#define KOFFICE_VERSION_MINOR 3
 
 /**
  * @def KOFFICE_VERSION_RELEASE
  * @ingroup KOfficeMacros
  * @brief Release version of KOffice, at compile time
 */
-#define KOFFICE_VERSION_RELEASE 84
+#define KOFFICE_VERSION_RELEASE 71
 
 /**
  * @ingroup KOfficeMacros
@@ -62,7 +62,7 @@
  *
  * This function can be used for preprocessing when KOFFICE_IS_VERSION is not
  * appropriate.
- */ 
+ */
 #define KOFFICE_MAKE_VERSION( a,b,c ) (((a) << 16) | ((b) << 8) | (c))
 
 /**
@@ -70,14 +70,14 @@
  * @ingroup KOfficeMacros
  * @brief If defined, indicates at compile time that KOffice is in alpha stage
  */
-// #define KOFFICE_ALPHA 1
+#define KOFFICE_ALPHA 1
 
 /**
  * @def KOFFICE_BETA
  * @ingroup KOfficeMacros
  * @brief If defined, indicates at compile time that KOffice is in beta stage
  */
-#define KOFFICE_BETA 4
+// #define KOFFICE_BETA 4
 
 /**
  * @def KOFFICE_RC
