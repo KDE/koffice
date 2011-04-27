8f4f309e5cdaf008328a4f0d9ebe162970b6e4b5
Pino Toscano <pino@kde.org>
2011-04-27 14:18:04 +0200

    karbon/tools: Messages.sh: KarbonGradientEditWidget.cpp does not exist anymore

--- karbon/plugins/tools/Messages.sh
+++ karbon/plugins/tools/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 $EXTRACTRC ../../ui/widgets/KarbonPatternOptionsWidget.ui filterEffectTool/FilterEffectEditWidget.ui >> rc.cpp
 $XGETTEXT rc.cpp *.cpp *.h CalligraphyTool/*.cpp CalligraphyTool/*.h filterEffectTool/*cpp ../../ui/widgets/KarbonPatternOptionsWidget.cpp \
-../../ui/widgets/KarbonGradientEditWidget.cpp -o $podir/KarbonTools.pot
+-o $podir/KarbonTools.pot
