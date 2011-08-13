1f44762d4c07b86f25d2a6c9249532871a79dc3e
Thomas Zander <zander@kde.org>
2011-08-13 17:00:35 +0200

    API docs

--- libs/flake/KToolBase.h
+++ libs/flake/KToolBase.h
@@ -42,16 +42,69 @@ class QPainter;
 class QInputMethodEvent;
 
 /**
- * Abstract base class for all tools. Tools can create or manipulate
- * flake shapes, canvas state or any other thing that a user may wish
- * to do to his document or his view on a document with user input.
- * also refer to kinteractiontool
+ * Abstract base class for all tools.
+ * In its simplest form a tool is a class that gets routed all the user input (mouse / keybard / tablet)
+ * and it can handle that to implement your application behavior.
  *
- *  [list copy paste methods]
- *    [explain KToolSelection and the setter/getter]
- *  [mention that a tool paints over its shapes]
- *  [mention that keyboard input is optional and has to be enabled]
- *  [explain read/write concept]
+ * Whereas in most applications this is done in the user interface, in flake the content the user has
+ * on screen is used to select a tool and a tool implementation can therefor be much more specific and limited
+ * in what kind of user input it will handle.
+ *
+ * As an example of how the tools concept fits in the whole of the application concept, consider this example.
+ * A PDF reader allows the user to navigate over the pages either by pressing the cursor keys
+ * or by clicking-and-dragging the mouse.
+ * There is a special button that when instead of draging the page, the cursor will select the text on screen
+ * and puts it in the clipboard.  From a interaction design point of view this means that the application has
+ * two modes, a navigation mode and a selection mode.  Because based on the mode the mouse handling will have
+ * different effects.
+ * If we map this onto Flake and tools we will end up with 2 tools. One per mode.  The tool that is active will
+ * handle the mouse events and take the actions.   The advantage of using two tools is that its got a better
+ * separation of concerns.  The two ways of working are put into different classes.
+ *
+ * The interaction design done for flake using applications doesn't just stop at modes, it goes a step further
+ * by making the content shown on screen (image, text, etc) determine which tools will be presented to the user.
+ * If there are no images, the tool that handles images will never be seen by the user and thus make choosing
+ * between tools easier.
+ * When we mention 'content' in this context we are specifically refering to KShape inheriting classes that
+ * display the content.
+ *
+ * The KToolBase class goes together with the KToolFactoryBase in configuring which tool to show for which
+ * type of content.  Specifically the setting of the KToolFactoryBase::activationShapeId() to the relevant
+ * choice.  Its also relevant to notice that a tool is matched to a factory using the toolId().
+ *
+ * Each tool is, as mentioned above, a mode. The user will specifically switch between those modes and although
+ * there are various features that will make that as painless as possible, there is the chance that we want
+ * to have different behavior while not switching away from the current tool.  For example if we are interacting
+ * with a virtual bicycle then when the user clicks and drags one of the tires we may want to show an animation.
+ * But if the user clicks on the chassis we may want to change the viewpoint and rotate the whole bike around.
+ * This kind of situation where the actual interaction is decided only at the point of the user clicking somewhere
+ * is made easy using the KInteractionTool subclass. The different sub-modes are implemented using the strategy
+ * design pattern.
+ *
+ * Tools handle low level user input like mouse events by reimplementing methods like mousePressEvent(),
+ * keyPressEvent() and wheelEvent().  A tool by default will only receive mouse events and using the Flag enums
+ * and the setFlags() methods can enable more types. See for example the ToolHandleKeyEvents enum value.
+ *
+ * Tools handle higher level input using KAction objects and addAction() which allows tools to have keyboard
+ * shortcuts and menu entries using a common implementation pattern.
+ * Default actions already pre-provided are deleteSelection(), copy() and paste(), those methods can be reimplemented
+ * by tools.  They go together with a KToolSelection class which allows the tool to make sure the cut/copy
+ * actions are only enabled when the tool actually has a selection.
+ *
+ * Next to handling user input, the tool handles some more functions that allow it to be useful as well as
+ * powerful.  One of those features is that the tool is allowed to paint on top of the canvas with complete
+ * freedom.  This allows tools to draw hints for user interaction.  For example a circle where the user can
+ * start a drag on a vector shape.
+ *
+ * Tools have full freedom on what they do with the user input.  Typically a tool provides a combination of
+ * navigation and editing functionlity.  A text tool will allow the cursor keys to navigate but typing text
+ * will modify.   When the application using the tool opens a document that can not be modified (for example
+ * a document that is opened from a remote website over http) it may be disirable to disallow editing
+ * the document until the user choose to save it locally.  This avoids the user putting in time in editing
+ * only later to find out she can't use those modifications.
+ * In KToolBase this usecase is called ReadWrite and tool implementors can use this by calling isReadWrite()
+ * in their reimplementations of the event handling methods and by using the ReadWrite enum when adding
+ * actions.
  *
  * There exists an instance of every tool for every pointer device.
  * These instances are managed by the KToolManager..
@@ -274,6 +327,14 @@ public:
      */
     virtual void deactivate();
 
+    /**
+     * Returns this tools's flags. The flags describe what configurable features of the tool are enabled and not.
+     * For example, if the flags include ToolHandleKeyEvents, the item can accept key events.
+     * By default, no flags are enabled.
+     * @see setFlags() and setFlag().
+     */
+    Flags flags() const;
+
     /// \internal
     KToolBasePrivate *priv();
 
@@ -393,6 +454,26 @@ protected:
     */
     QRectF handlePaintRect(const QPointF &position) const;
 
+    /**
+     * On every keypress the tool is allowed to steal it before anyone else sees it.
+     *
+     * On every keypress this method is called on the active tool to allow to tool to handle
+     * the key and / or shortcut before the normal chain of event handling kicks in.
+     * If the tool decides to accept the event this will cause none of the actions to be
+     * matched in the whole application.
+     *
+     * The main reason tools may want to enable this is when they suspect an application having a KAction
+     * that is triggered by a keyboard shortcut and the tool listens to the same key and never receives it.
+     * For example if an application registers 'select-all' as a default shortcut and the tool does the same.
+     * Another example is when the application has single-key shortcuts to activate functionality but the
+     * tool is meant to be used to type text.
+     * In both cases the action being registered in the application prevent the tool from getting it via
+     * the normal way and the tool can override this default behavior by accepting the specic key in this
+     * method.
+     *
+     * Tools that enable ToolHandleShortcutOverride in setFlag() will get these events offered,
+     * otherwise they risk actions set in the application to steal key events it would normally get.
+     */
     virtual void shortcutOverride(QKeyEvent *event);
 
     /**
@@ -467,9 +548,19 @@ protected:
      */
     virtual void inputMethodEvent(QInputMethodEvent *event);
 
+    /**
+     * Sets the shape flags to flags.
+     * All flags in flags are enabled; all flags not in flags are disabled.
+     * By default, no flags are enabled. (KInteractionTool enables the ToolHandleKeyEvents flag by default)
+     * @see flags() and setFlag().
+     */
     void setFlags(Flags flags);
-    void setFlag(Flag flag, bool on = true);
-    Flags flags() const;
+
+    /**
+     * If enabled is true, the item flag flag is enabled; otherwise, it is disabled.
+     * @see flags() and setFlags().
+     */
+    void setFlag(Flag flag, bool enabled = true);
 
     /// \internal
     KToolBase(KToolBasePrivate &dd);
