8a181fd7f828cc55d1f2ffe28a72bd70bd095db7
Albert Astals Cid <tsdgeos@terra.es>
2011-08-28 21:44:44 +0200

    Fix Messages.sh
    If you remove the variable it is a good idea to also remove its uses
    CCMAIL: zander@kde.org

--- libs/Messages.sh
+++ libs/Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-LIST=`find . \( \( $EXCLUDE \) -prune -o -name \*.ui -o -name \*.rc \) -type f | grep -v -e '/\.'`
+LIST=`find . \( -name \*.ui -o -name \*.rc \) -type f | grep -v -e '/\.'`
 $EXTRACTRC $LIST >> rc.cpp
-LIST=`find . \( \( $EXCLUDE \) -prune -o -name \*.cpp -o -name \*.cc -o -name \*.h \) -type f | grep -v '/tests/' | grep -v -e '/\.'`
+LIST=`find . \( -name \*.cpp -o -name \*.cc -o -name \*.h \) -type f | grep -v '/tests/' | grep -v -e '/\.'`
 $XGETTEXT $LIST -o $podir/koffice.pot
